93f3179a4584b8b38f3ee898d4ab4f53d1ed22ec
ionelabejan1901-wq <ionelabejan1901@gmail.com>
2025-11-26 14:50:30 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace BejanIonelaLab7
+﻿using System;
+using System.IO;
+using BejanIonelaLab7.Data;
+
+
+namespace BejanIonelaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
